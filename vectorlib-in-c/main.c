e67ab284aebd1d06a40a411b61c2af75c9e1f534
Felipe <felipe.loose@gmail.com>
2023-07-09 13:12:49 +0200

    added main.c to test inner product function

--- vectorlib-in-c/main.c
+++ vectorlib-in-c/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "vectorLib.h"
 
-int main()
+
+int main(int argc, char *argv[])
 {
     
+    //creates vectors
+    int size;
+    size = 2000;
+    struct Vector *x = vector_create("X",size);
+    struct Vector *y = vector_create("Y",size);
+
+    //assigns them functions (cosine and sine)
+    vector_create_cos(x,0,50);
+    vector_create_sin(y,0,50);
+
+    //computes inner product
+    float *inner_p;
+    inner_p = malloc(sizeof(float));
+    vector_inner_product(y,y,inner_p);
+    printf("Calculated Inner Product: %f\n",*inner_p);
+
+    //call free and destroy of the pointers
+    vector_destroy(x);
+    vector_destroy(y);
+    free(inner_p);
 }
+
