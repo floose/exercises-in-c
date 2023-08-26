451f1a670ff688a748736184abca0e25453025e9
Felipe <felipe.loose@gmail.com>
2023-08-26 23:05:25 +0200

    added main function as an example

--- mathematics_exercises/array_class.cpp
+++ mathematics_exercises/array_class.cpp
@@ -12,9 +12,9 @@ private:
 public:
     Array()
     {
-        Size=10;
+        Size = 10;
         A = new T[10];
-        Length=0;
+        Length = 0;
     }
     Array(int size)
     {
@@ -24,51 +24,62 @@ public:
     }
     ~Array()
     {
-        delete []A;
+        delete[] A;
     }
     void Display();
     void Insert(int index, T value);
     T Delete(int index);
 };
 
-template<class T>
+template <class T>
 void Array<T>::Display()
 {
-    
-    for(int i = 0; i < Length ; i++)
-    [
-        cout << A[i] << " ";
-        cout << endl;
-    ]
+
+    for (int i = 0; i < Length; i++)
+        [cout << A[i] << " ";
+            cout << endl;
+        ]
 }
 
-template<class T>
+template <class T>
 void Array<T>::Insert(int index, T value)
 {
-    //guards for size
-    if(index >= 0 && index <= Lenght)
+    // guards for size
+    if (index >= 0 && index <= Lenght)
     {
-        //shifts everyone to create space
-        for(int i = Length-1 ; i >= index ; i--)
-            A[i+1] = A[i];
+        // shifts everyone to create space
+        for (int i = Length - 1; i >= index; i--)
+            A[i + 1] = A[i];
         A[index] = value;
         Length++;
     }
 }
 
-template<class T>
+template <class T>
 T Array<T>::Delete(int index)
 {
     T aux = 0;
 
-    //guards for size
-    if(index >= 0 && index < Length)
+    // guards for size
+    if (index >= 0 && index < Length)
     {
-        aux = A[index]; 
-        //shifts everyone to shrink array
-        for(int i = index ; i < Length-1 ; i++)
-            A[i] = A[i+1];
-        Length--; //decreases length
+        aux = A[index];
+        // shifts everyone to shrink array
+        for (int i = index; i < Length - 1; i++)
+            A[i] = A[i + 1];
+        Length--; // decreases length
     }
     return aux;
+}
+
+int main()
+{
+    Array<char> arr(10);
+    arr.Insert(0, 'a');
+    arr.Insert(1, 'c');
+    arr.Insert(2, 'd');
+    arr.Display();
+    cout << arr.Delete(0) << endl;
+    arr.Display();
+    return 0;
 }
